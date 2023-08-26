1ad33d2532338d387aca05b0654de2857bd472eb
hutu <1048672466@qq.com>
2023-08-26 17:29:17 +0800

    feat: 添加微信公众号排版

---
+++ vue3-markdown-wechat/assets/Index.3fa52079.js
@@ -0,0 +1,101 @@
+import{d as i,r as s,t as B,a as c,o as d,c as r,b as C,u as t,i as A}from"./index.8eb19598.js";const D={class:"index"},f=i({__name:"Index",setup(m){const F=s({isFullscreen:!0,content:`>**\u5FAE\u4FE1\u516C\u4F17\u53F7\uFF1ARMRF**
+\u5173\u6CE8\u53EF\u4E86\u89E3\u66F4\u591A\u7684\u6559\u7A0B\uFF0C\u95EE\u9898\u6216\u5EFA\u8BAE\uFF0C\u8BF7\u516C\u4F17\u53F7\u7559\u8A00;
+
+\u8BE5\u5DE5\u5177\u4F7F\u7528\u4E86\u3010v-md-editor\u3011\u7EC4\u4EF6\u5E93\u6765\u5B9E\u73B0\uFF0C\u4E3B\u8981\u7528\u4E8E\u516C\u4F17\u53F7\u7684\u5185\u5BB9\u6392\u7248\u3001\u4EE3\u7801\u9AD8\u4EAE\u3002\u76EE\u524D\u53EA\u7A0D\u5FAE\u4FEE\u6539\u4E86\u4E3B\u9898\u6837\u5F0F\uFF0C\u5982\u679C\u6709\u559C\u6B22\u7684\u4E3B\u9898\u53EF\u4EE5\u5728\u516C\u4F17\u53F7\u7559\u8A00\uFF0C\u540E\u7EED\u4F1A\u51FA\u76F8\u5E94\u7684\u81EA\u5B9A\u4E49\u6837\u5F0F\u3001\u6392\u7248\u98CE\u683C\u7B49\u529F\u80FD\u3002
+
+### \u6587\u5B57
+
+**\u6211\u5DF2\u7ECF\u52A0\u7C97\u4E86**
+
+*\u6211\u662F\u659C\u4F53*
+
+~~\u6211\u662F\u5220\u9664\u7EBF~~
+
+### \u5217\u8868
+
+- \u65E0\u5E8F\u5217\u8868
+- \u65E0\u5E8F\u5217\u8868
+- \u65E0\u5E8F\u5217\u8868
+
+1. \u6709\u5E8F\u5217\u8868
+2. \u6709\u5E8F\u5217\u8868
+3. \u6709\u5E8F\u5217\u8868
+
+### \u5F15\u7528\u5757
+
+>\u6211\u662F\u5F15\u7528\u5757
+\u6BCF\u5929\u8FDB\u6B65\u4E00\u70B9\u70B9
+
+### \u6807\u9898
+
+\u652F\u63016\u79CD\u5927\u5C0F\u7684\u6807\u9898\uFF0C\u5206\u522B\u5BF9\u5E94\`#\`,\`##\`,\`###\`,\`####\`,\`#####\`,\`######\`\u5982\uFF1A
+
+#### \u56DB\u7EA7\u6807\u9898
+##### \u4E94\u7EA7\u6807\u9898
+###### \u516D\u7EA7\u6807\u9898
+
+### \u56FE\u7247
+
+\u53EF\u4EE5\u4F7F\u7528\`![Description](https://)\`\u6765\u63D2\u5165\u56FE\u7247\u94FE\u63A5
+
+![Description](https://www.lpya.cn/static/uploads/20230822/6cbe0e9390dbff486c0a45ce39e50479.png)
+
+### \u4EE3\u7801
+
+\u56E0\u4E3A\u516C\u4F17\u53F7\u81EA\u5E26\u7684\u4EE3\u7801\u9AD8\u4EAE\u6BD4\u8F83\u5355\u4E00\u65E0\u6CD5\u6EE1\u8DB3\u9700\u6C42\uFF0C\u81EA\u5DF1\u53EF\u80FD\u9700\u8981\u81EA\u5B9A\u4E49\u4E00\u4E9B\u6837\u5F0F\uFF0C\u5982\u6697\u9ED1\u6A21\u5F0F\u7B49\u3002\u90A3\u5C31\u9700\u8981\u4F7F\u7528\u5916\u90E8\u63D2\u4EF6\u6765\u8FBE\u5230\u81EA\u5DF1\u60F3\u8981\u7684\u6548\u679C\u4E86\u3002
+
+**HTML**
+
+\`\`\`html
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Hello World!</title>
+  <style>
+    #app {
+      padding: 0px;
+      margin: 0px;
+      font-size: 20px;
+    }
+  </style>
+</head>
+
+<body>
+  <div id="#app">
+    <button onclick="onclick">Hello World!</button>
+  </div>
+</body>
+
+</html>
+\`\`\`
+
+**golang**
+\`\`\`go
+package main
+
+import "fmt"
+
+func main()  {
+  fmt.Println("Hello World!")
+}
+\`\`\`
+
+**SQL\u8BED\u53E5**
+\`\`\`sql
+SELECT id,title FROM article WHERE id = 1;  
+\`\`\`
+
+### \u8868\u683C
+
+|ID|\u6587\u7AE0\u540D\u79F0|
+|-|-|
+|1|threejs\u5B9E\u73B0\u5730\u94C1\u7EBF\u8DEF\u56FE\u6D41\u5149\u6548\u679C|
+|2|threejs\u5B9E\u73B0\u5730\u94C1\u7EBF\u8DEF\u56FE\u6D41\u5149\u6548\u679C|
+|3|threejs\u5B9E\u73B0\u5730\u94C1\u7EBF\u8DEF\u56FE\u6D41\u5149\u6548\u679C|
+
+>**\u6E29\u99A8\u63D0\u793A**
+\u76EE\u524D\u7F16\u8F91\u7684\u5185\u5BB9\u4F1A\u81EA\u52A8\u4FDD\u5B58\u5230\u6D4F\u89C8\u5668\u7F13\u5B58\`localStorage\`\uFF0C\u4F46\u91CD\u8981\u5185\u5BB9\u8BF7\u8BB0\u5F97\u505A\u5907\u4EFD
+`}),{isFullscreen:o,content:E}=B(F);(()=>{const u=localStorage.getItem("content");u&&(F.content=u)})();const l=(u,e)=>{localStorage.setItem("content",u)};return(u,e)=>{const n=c("v-md-editor");return d(),r("div",D,[C(n,{modelValue:t(E),"onUpdate:modelValue":e[0]||(e[0]=a=>A(E)?E.value=a:null),"disabled-menus":[],"default-fullscreen":t(o),onChange:l,"left-toolbar":"undo redo clear | h bold italic strikethrough quote | ul ol table hr | link image code | customToolbar1"},null,8,["modelValue","default-fullscreen"])])}}});export{f as default};
